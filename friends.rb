49fc7f8747c1546204d62ff01759a4379a940668
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2022-10-19 20:52:41 -0700

    Add a very good friend

--- friends.rb
+++ friends.rb
@@ -1,6 +1,11 @@
-best_friend = "Jerry"
+best_friend = "Jess"
+very_good_friend = "olan"
+
 if best_friend == "Rudy"
-    puts "Hey there, best friend!"
-elsif best_friend != "Rudy"
+    puts "Hey there, Rudy!"
+elsif best_friend != "Rudy" && very_good_friend == "Nolan"
+    puts "Hey there, Nolan!"
+elsif very_good_friend != "Nolan"
     puts "Hello, friend!"
 end
+ 
